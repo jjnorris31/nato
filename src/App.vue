bce3c5e0cc301d9431f59a287c6b038d3eef9afc
jjnorris31 <jjnorris31@gmail.com>
2020-02-28 15:30:50 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar app color="white" flat>
+      <v-toolbar-title class="toolbar">Nato</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon><v-icon :color="iconColor">mdi-magnify</v-icon></v-btn>
+      <v-btn icon><v-icon :color="iconColor">mdi-dots-vertical</v-icon></v-btn>
+    </v-app-bar>
+    
+    <v-content>
+      <v-container fluid>
+        <v-row>
+          <Notes :pages="pages" @new-note="newNote"/>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+<script>
+    import Notes from './components/Notes.vue'
+    export default {
+        name: 'app',
+        components: {
+            Notes
+        },
+        data: () => ({
+            pages:[],
+            index:0,
+            iconColor: '#6A6E74',
+        }),
+        methods:  {
+            newNote () {
+                this.pages.push({
+                    title: "",
+                    content: ""
+                });
+                console.log(this.pages);
+            },
+            saveNote () {
+            },
+            deleteNote () {
+            }
+        }
+    }
+</script>
+<style>
+  
+  :root {
+    --main-font-color: #6A6E74;
+  }
+  
+  .toolbar {
+    font-family: Lato, sans-serif;
+    font-weight: bold;
+    color: var(--main-font-color)
+  }
+</style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="12">
+    <v-row justify="center"
+           no-gutters>
+      <v-col cols="10">
+        <h1 class="n1 mt-4 mb-2">Mis notas</h1>
+      </v-col>
+      <v-col cols="10">
+        <v-list two-line class="text-left">
+          <v-list-item v-for="n in 5" class="mb-2" :key="n" :style="{ backgroundColor: getRandomColor()}">
+            <v-list-item-content>
+              <v-list-item-title>Nota #{{n}}</v-list-item-title>
+              <v-list-item-subtitle>Esta es la nota {{n}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+    </v-col>
+    <v-btn icon
+           absolute
+           bottom
+           right
+           x-large
+           color="white"
+           @click="getRandomColor"
+           style="background-color: #33B5FB"
+           elevation="5">
+      <v-icon large>mdi-plus</v-icon>
+    </v-btn>
+  </v-row>
+</template>
+<script>
+    export default {
+        name: 'Notes',
+        props: ['pages'],
+        data: () => ({
+            noteColors: ['#DEF2FE', '#D2F3D1', '#FEECDE', '#F7F6C5'],
+        }),
+        methods: {
+            newNote () {
+                this.$emit('new-note')
+            },
+            getRandomColor() {
+                let color = this.noteColors[Math.floor(Math.random() * 3)];
+                console.log(color);
+                return color;
+            }
+        }
+    }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .full-height {
+    height: 100%;
+  }
+  
+  .n1 {
+    font-weight: lighter;
+    font-family: Lato, sans-serif;
+    font-size: 30px;
+    color: #33B5FB;
+  }
+  
+  .note-item {
+  }
+</style>
